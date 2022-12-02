970598bf493bd2e76e562c6db485766039630665
smithf6 <114438095+smithf6@users.noreply.github.com>
2022-12-02 01:01:30 -0800

    Add files via upload

---
+++ descriptionsB.js
@@ -0,0 +1,68 @@
+$(document).ready(function() {
+  
+  
+    $("#BT1C").click(showBT1);
+    $("#BT2C").click(showBT2);
+    $("#BT3C").click(showBT3);
+    $("#BT4C").click(showBT4);
+    $("#BM1C").click(showBM1);
+    $("#BM2C").click(showBM2);
+    $("#BM3C").click(showBM3);
+    $("#BM4C").click(showBM4);
+    $("#BL1C").click(showBL1);
+    $("#BL2C").click(showBL2);
+    $("#BL3C").click(showBL3);
+    $("#BL4C").click(showBL4);
+   
+  
+    
+    function showBT1() {
+      $('.descriptions_B').hide();
+      $('#BT1').show();
+    }
+    function showBT2() {
+        $('.descriptions_B').hide();
+        $('#BT2').show();
+      }
+      function showBT3() {
+        $('.descriptions_B').hide();
+        $('#BT3').show();
+      }
+      function showBT4() {
+        $('.descriptions_B').hide();
+        $('#BT4').show();
+      }
+      function showBM1() {
+        $('.descriptions_B').hide();
+        $('#BM1').show();
+      }
+      function showBM2() {
+        $('.descriptions_B').hide();
+        $('#BM2').show();
+      }
+      function showBM3() {
+        $('.descriptions_B').hide();
+        $('#BM3').show();
+      }
+      function showBM4() {
+        $('.descriptions_B').hide();
+        $('#BM4').show();
+      }
+      function showBL1() {
+        $('.descriptions_B').hide();
+        $('#BL1').show();
+      }
+      function showBL2() {
+        $('.descriptions_B').hide();
+        $('#BL2').show();
+      }
+      function showBL3() {
+        $('.descriptions_B').hide();
+        $('#BL3').show();
+      }
+      function showBL4() {
+        $('.descriptions_B').hide();
+        $('#BL4').show();
+      }
+
+  });
